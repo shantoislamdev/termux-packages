2c55c1e34596daccf244fcdbce35ea0189c96ca5
Shanto Islam <shantoislamdev@gmail.com>
2026-08-10 22:56:17 +0600

    Fix formatting of TERMUX_SUBPKG_DESCRIPTION

--- packages/libgnutls/libgnutls-dane.subpackage.sh
+++ packages/libgnutls/libgnutls-dane.subpackage.sh
@@ -1,4 +1,3 @@
 TERMUX_SUBPKG_INCLUDE="include/gnutls/dane.h lib/libgnutls-dane.so lib/pkgconfig/gnutls-dane.pc"
 TERMUX_SUBPKG_DEPENDS="libunbound"
-TERMUX_SUBPKG_DESCRIPTION="DANE protocol implementation for GnuTLS
-Commandline utilities for interfacing with the gnutls library"
+TERMUX_SUBPKG_DESCRIPTION="DANE protocol implementation for GnuTLS Commandline utilities for interfacing with the gnutls library"
